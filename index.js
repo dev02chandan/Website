301d0a80b433f32eb8e340743b1749396cc5a9c2
dev02chandan <dev02chandan@gmail.com>
2024-06-24 16:41:50 +0530

    Basic website

--- index.js
+++ index.js
@@ -12,15 +12,18 @@ document.querySelectorAll("nav a").forEach((anchor) => {
 window.addEventListener("load", function () {
   const greetingPopup = document.getElementById("greeting-popup");
   const overlay = document.getElementById("overlay");
+  const body = document.body;
 
   // Show the greeting and overlay
   greetingPopup.style.opacity = "1";
   overlay.style.opacity = "1";
+  body.classList.add("show-border");
 
   // Hide the greeting and overlay after 3 seconds
   setTimeout(() => {
     greetingPopup.style.opacity = "0";
     overlay.style.opacity = "0";
+    body.classList.remove("show-border");
 
     // Remove the elements from the DOM after the fade-out
     setTimeout(() => {
